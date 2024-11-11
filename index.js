36d31d6124e9571d7e245987b1fff698d0574588
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-11-11 11:21:04 +0100

    Solution: Storing short_url and check

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
 const dns = require('node:dns');
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const URLSchema = new mongoose.Schema({
+    original_url: { type: String, required: true, unique: true },
+    short_url: { type: String, required: true, unique: true },
+})
+
+let URLModel = mongoose.model('URLModel', URLSchema);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,15 +28,31 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.get('/api/shorturl/:short_url', function (req, res) {
+    let short_url = req.params.short_url;
+    // Find the original URL from the database using the short_url
+    URLModel.findOne({ short_url: short_url })
+        .then((foundURL) => {
+            console.log(foundURL);
+            if (foundURL) {
+                let original_url = foundURL.original_url;
+                res.redirect(original_url);
+            } else {
+                res.json({ message: 'The shorturl doesnt exist!' });
+            }
+        })
+        .catch((err) => {
+            res.json({ error: 'An error occurred' });
+        });
+});
+
 // Your first API endpoint
 app.post('/api/shorturl', function (req, res) {
-
     let url = req.body.url;
 
     // Validate the URL
     try {
-        urlObj = new URL(url);
-        console.log(urlObj)
+        let urlObj = new URL(url);
         dns.lookup(urlObj.hostname, (err, address, family) => {
             console.log(address);
 
@@ -39,8 +63,15 @@ app.post('/api/shorturl', function (req, res) {
             // We have a valid URL
             else {
                 let original_url = urlObj.href;
-                let shorturl = 1;
-                res.json({ original_url: original_url, short_url: shorturl });
+                let short_url = 1
+                let resObj = {
+                    original_url: original_url,
+                    short_url: short_url
+                }
+                // Create an entry in the database
+                let newURL = new URLModel(resObj);
+                newURL.save()
+                res.json(resObj);
             }
         })
     }
