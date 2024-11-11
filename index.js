337b3d6d327a6a9adaa5808aefe8f10487619983
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-11-11 10:59:23 +0100

    Solution - URL validation

--- index.js
+++ index.js
@@ -2,23 +2,53 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+const dns = require('node:dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function (req, res) {
+
+    let url = req.body.url;
+
+    // Validate the URL
+    try {
+        urlObj = new URL(url);
+        console.log(urlObj)
+        dns.lookup(urlObj.hostname, (err, address, family) => {
+            console.log(address);
+
+            // If the DNS domain does not exist no address
+            if (!address) {
+                res.json({ error: 'invalid url' });
+            }
+            // We have a valid URL
+            else {
+                let original_url = urlObj.href;
+                let shorturl = 1;
+                res.json({ original_url: original_url, short_url: shorturl });
+            }
+        })
+    }
+    catch {
+        res.json({ error: 'invalid url' });
+    }
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
 });
